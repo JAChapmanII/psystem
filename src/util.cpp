434bb2e463de1a5515bf3e500d28e614ea0ad05c
Jeff Chapman II <jac@jac2.net>
2017-04-15 14:56:35 -0400

    Update help and README to mention --load switch

--- src/util.cpp
+++ src/util.cpp
@@ -5,9 +5,10 @@ using std::endl;
 #include <cmath>
 
 void util::outputHelp(ostream &out) {
-	out << "psystem: An implementation of a simplectic Euler integrator" << endl;
-	out << "\t-h|--help: print this help message and quit" << endl;
-	out << "see the README for more information" << endl;
+	out << "psystem: An implementation of a simplectic Euler integrator" << endl
+		<< "\t-h|--help: print this help message and quit" << endl
+		<< "\t   --load: load a particle system from the file specified" << endl
+		<< "see the README for more information" << endl;
 }
 
 ldouble util::signum(ldouble &x) {
